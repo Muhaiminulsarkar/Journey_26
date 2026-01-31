5e12f176ad8b89cb5d46bf513afe37ce09326d1f
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2026-01-31 19:01:26 +0600

    bbasic of array added

---
+++ All-Volume/Volume_three/js-array/pop.js
@@ -0,0 +1,16 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+// friends.push('khailam');
+console.log(friends)
+
+const out1 = friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(friends);
+console.log(out1, out2);
+friends.push('holam')
+console.log(friends);
+friends.pop();
+console.log(friends);
+
+
+
