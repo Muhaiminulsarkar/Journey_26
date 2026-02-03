3ff0de7a8ed3bc0264615c87c05b47c10362f4ef
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2026-02-04 02:16:48 +0600

    loop basic code

---
+++ All-Volume/Volume_three/JS-loop/for-sum.js
@@ -0,0 +1,18 @@
+let sum = 0; 
+
+for(let i = 11; i<=20; i++){    
+    sum = sum + i;
+    console.log(i);
+}
+
+console.log('sum of numbers from 11 to 20 is', sum) 
+
+
+let sum2 = 0;
+let p = 1
+while(p<=10){
+    console.log(p);
+    sum2 += p;
+    p++
+}
+console.log(sum2);
